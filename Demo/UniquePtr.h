e228126f9b47a2aa335da79221d485eb3306bc23
MarinaPaley <marina_paley@mail.ru>
2024-10-11 15:57:49 +0300

    impementing

--- Demo/UniquePtr.h
+++ Demo/UniquePtr.h
@@ -1,5 +1,78 @@
 #pragma once
 namespace SmartPtr
 {
+	template <typename T>
+	class UniquePtr final
+	{
+	private:
+		T* ptr;
+	public:
+		UniquePtr(const T value);
+		~UniquePtr();
+		UniquePtr(const UniquePtr& other) = delete;
+		UniquePtr(UniquePtr&& other) noexcept;
+		UniquePtr& operator=(const UniquePtr& other) = delete;
+		UniquePtr& operator=(UniquePtr&& other) noexcept;
+		T Get() const;
+		void Swap(UniquePtr& other) noexcept;
+		void Reset(UniquePtr& other) noexcept;
+		T* Release();
+	};
 
+	template<typename T>
+	inline UniquePtr<T>::UniquePtr(const T value)
+		: ptr{ new T{value} }
+	{
+	}
+
+	template<typename T>
+	inline UniquePtr<T>::~UniquePtr()
+	{
+		delete this->ptr;
+	}
+
+	template<typename T>
+	inline UniquePtr<T>::UniquePtr(UniquePtr&& other) noexcept
+		: ptr {nullptr}
+	{
+		*this = std::move(other);
+	}
+
+	template<typename T>
+	inline UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr<T>&& other) noexcept
+	{
+		if (this != &other)
+		{
+			std::swap(this->ptr, other.ptr);
+		}
+
+		return *this;
+	}
+
+	template<typename T>
+	inline T UniquePtr<T>::Get() const
+	{
+		return *(this->ptr);
+	}
+	template<typename T>
+	inline void UniquePtr<T>::Swap(UniquePtr& other) noexcept
+	{
+		std::swap(this->ptr, other.ptr);
+	}
+
+	template<typename T>
+	inline void UniquePtr<T>::Reset(UniquePtr& other) noexcept
+	{
+		delete this->ptr;
+		this->ptr = other.ptr;
+		other.ptr = nullptr;
+	}
+
+	template<typename T>
+	inline T* UniquePtr<T>::Release()
+	{
+		auto temp = this->ptr;
+		this->ptr = nullptr;
+		return temp;
+	}
 }
--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <memory>
+#include "UniquePtr.h"
+
+using namespace SmartPtr;
 int main()
 {
+	const int value = 42;
+	const int value2 = 1;
+	auto q = new int{ value };
+
+	UniquePtr<int> ptr{ value };
+	UniquePtr<int> ptr2{ value2 };
+	std::cout << ptr.Get() << " " << ptr2.Get() << std::endl;
+	ptr.Swap(ptr2);
+	std::cout << ptr.Get() << " " << ptr2.Get() << std::endl;
+
+	ptr.Reset(ptr2);
+	std::cout << ptr.Get() << std::endl;
+	delete q;
+
+	//std::unique_ptr<int> u_ptr1{ std::make_unique<int>(value) };
+	//std::cout << *u_ptr1.get() << std::endl;
 	return 0;
 }
